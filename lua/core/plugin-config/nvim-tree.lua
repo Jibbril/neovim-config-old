bb6ead92793e56ecfc9a7add63399241971cad94
Jibbril Berbres <jibbril.berbres@viego.se>
2023-04-09 21:49:31 +0200

    Add focus tree command

--- lua/core/plugin-config/nvim-tree.lua
+++ lua/core/plugin-config/nvim-tree.lua
@@ -5,4 +5,5 @@ require('nvim-tree').setup()
 require('nvim-web-devicons').setup()
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<leader>E', '::NvimTreeFocus<CR>')
 
