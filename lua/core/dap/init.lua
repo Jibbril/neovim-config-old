68ac5f05f8a978f75183a3c62d98af9777231333
Jibbril Berbres <jibbril.berbres@viego.se>
2023-04-10 17:37:02 +0200

    Move language DAP configs

--- lua/core/dap/init.lua
+++ lua/core/dap/init.lua
@@ -1,13 +1,2 @@
-require("dap").adapters["pwa-node"] = {
-  type = "server",
-  host = "localhost",
-  port = "${port}",
-  executable = {
-    command = "node",
-    -- 💀 Make sure to update this path to point to your installation
-    args = {"/home/jibbril/.local/share/nvim/site/pack/packer/opt/vscode-js-debug/out/src/dapDebugServer.js", "${port}"},
-  }
-}
-
 require('core.dap.keymaps')
-require('core.dap.javascript')
+require('core.dap.languages.javascript')
